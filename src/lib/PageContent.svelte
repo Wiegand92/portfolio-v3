3f850c13f19417445fdfad1bf2efb25022c85ba8
Wiegand92 <wiegand.alex@gmail.com>
2024-02-19 14:21:50 -0500

    Add presentational component

---
+++ src/lib/PageContent.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+    export let title = 'Page Name Here'
+</script>
+<h2>{title}</h2>
+<div>
+    <slot></slot>
+</div>
+<style>
+    h2{
+        place-self: center;
+    }
+</style>
